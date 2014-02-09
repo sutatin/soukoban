a8fc23151e2c0cdcc7e89e65c5f673c3521a0922
sutatin <sutatinsutatin@gmail.com>
2014-02-09 17:10:46 +0900

    making push & clear

--- soukoban.c
+++ soukoban.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
- /*
-    cout << "########" << endl;
-    cout << "# ..P  #" << endl;
-    cout << "# oo   #" << endl;
-    cout << "#      #" << endl;
-    cout << "########" << endl;
- */
 
     //
-    //残TODO
-    //クリアを作る
-    //数字じゃなくて文字で作る
-    //押してくと貫通するバグを直す
+    //TODO
+    //
+    //bug
+    //・clear when P on target
+    //・vanish after o through
+    //
+    //function
+    //・input figure
     //
 
     int y,x;
     enum nowField{normal,goal};
     enum nowField nowfieldType;
     int arrow;
+    int isFinish=0;
 
 //渡された配列のフィールドの描画
     void writeField(char witdh[7][9]){
@@ -63,6 +61,10 @@ void moveAction(char witdh[7][9],int y,int x,enum nowField e,int targetY, int ta
        int differenceX = targetX - x;
        int differenceY = targetY - y;
 
+
+       //canpush?
+       if(witdh[targetY+differenceY][targetX+differenceX]==' '
+          ||witdh[targetY+differenceY][targetX+differenceX]=='.'){
        //上向きならdifferenceY:-1
        //下向きならdifferenceY:1
        //左向きならdifferenceY:-1
@@ -72,8 +74,30 @@ void moveAction(char witdh[7][9],int y,int x,enum nowField e,int targetY, int ta
         witdh[targetY+differenceY][targetX+differenceX]='o';
         nowfieldType = normal;
     }
+    }
 };
 
+void checkFinish(char witdh[7][9]){
+     int i = 0;
+     int k = 0;
+     isFinish = 1;
+
+     while (i < 7){
+        while (k < 9){
+            char field = witdh[i][k];
+            if(field=='.'){
+                isFinish = 0;
+            }
+            k++;            
+        };
+        k= 0;
+        i++;
+    };
+
+    if(isFinish==1){
+        arrow = 6;
+    }
+}
 
 void init(char witdh[7][9]){
     writeField(witdh);
@@ -82,43 +106,44 @@ void init(char witdh[7][9]){
 
 int main(){
 
-    //start:
+    start:  printf("\n");
     char witdh[7][9] = {
         { '0','1', '2', '3', '4', '5', '6', '7', '8' }
         , { '1','#', '#', '#', '#', '#', '#', '#', '#' }
-        , { '2','#', ' ', '.', '.', 'P', ' ', ' ', '#' }
+        , { '2','#', ' ', '.', ' ', 'P', ' ', ' ', '#' }
         , { '3','#', ' ', 'o', 'o', ' ', ' ', ' ', '#' }
-        , { '4','#', ' ', ' ', ' ', ' ', ' ', ' ', '#' }
+        , { '4','#', ' ', ' ', ' ', '.', ' ', ' ', '#' }
         , { '5','#', ' ', ' ', ' ', ' ', ' ', ' ', '#' }
         , { '6','#', '#', '#', '#', '#', '#', '#', '#' } };
-        printf("\n");
 
         //初期処理
         init(witdh);
 
-        while(arrow!=5){
+        while(arrow!=6){
         //input
-            printf("移動コマンド　↑：1　↓：2　←：9 →：0　 終了：5\n");
+            printf("\n");
+            printf("移動コマンド　←：1　↑：2　↓：3　→：4　リセット：５　終了：６\n");
             scanf("%d",&arrow);
             printf("\n");
 
         //set targetXY from input
             int targetY,targetX;
-            if(arrow==1){
+            if(arrow==2){
                 targetY = y-1;
                 targetX = x;
-            }else if(arrow==2){
+            }else if(arrow==3){
                 targetY = y+1;
                 targetX = x;        
-            }else if(arrow==9){
+            }else if(arrow==1){
                 targetY = y;
                 targetX = x-1;        
-            }else if(arrow==0){
+            }else if(arrow==4){
                 targetY = y;
                 targetX = x+1;        
-            }else if(arrow == 6){
+            }else if(arrow == 5){
                 //リセット
-                //goto start;
+                printf("リセットしました。\n");
+                goto start;
             }else{
                 targetY = y;
                 targetX = x;                
@@ -126,7 +151,14 @@ int main(){
 
             moveAction(witdh,y,x,nowfieldType,targetY,targetX);
             writeField(witdh);
+            checkFinish(witdh);
 
+        }
+
+        if(isFinish==1){
+                printf("クリアおめでとう！\n");            
+        }else{
+                printf("終了します。\n");                        
         }
 
     };
